7d5e3b30daa0b2d3f6fff45447a4db81e57cf456
Alejo-P <marcelo.pinzon@epn.edu.ec>
2025-02-26 20:27:44 -0500

    Actualizar configuración del servidor para producción y ajustar rutas de carpetas; mejorar la estructura del proyecto.

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,17 @@
-# Configuración del servidor
-DEBUG = True
+import os
+
+# Configuración del servidor (para producción)
+DEBUG = False
 PORT = 5000
 HOST = "0.0.0.0"
 
+BASE_DIR = os.path.expanduser("~")  # Directorio base (donde se crearan y guardaran los archivos)
+
+APP_DIR = os.path.join(BASE_DIR, 'DocTools')  # Directorio de la aplicación
+
 # Configuración de archivos
-UPLOAD_FOLDER = './uploads'
-STATIC_FOLDER = './static'
-QR_FOLDER = './qrcode'
+UPLOAD_FOLDER = os.path.join(APP_DIR, 'uploads')  # Carpeta de archivos subidos
+STATIC_FOLDER = os.path.join(APP_DIR, 'static')  # Carpeta de archivos estáticos
+QR_FOLDER = os.path.join(APP_DIR, 'qr')  # Carpeta de códigos QR
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # 16 MB
