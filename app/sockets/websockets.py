d5c5848da731a2c34235928c913ae7a85695cc7d
Alejo-P <marcelo.pinzon@epn.edu.ec>
2025-05-25 08:23:02 +0000

    Refactor y mejora de la gestión de conexiones WebSocket

--- app/sockets/websockets.py
+++ app/sockets/websockets.py
@@ -16,23 +16,49 @@ class ConnectionManager:
         self.active_connections: List[WSConnection] = []
 
     async def connect(self, websocket: WebSocket, user_id: int, roles: List[str]):
-        #await websocket.accept()
-        self.active_connections.append(
-            WSConnection(websocket=websocket, user_id=user_id, roles=roles)
-        )
+        await self.disconnect_by_user(user_id)
+
+        connection = WSConnection(websocket, user_id=user_id, roles=roles)
+        self.active_connections.append(connection)
+        print(f"[WS] Conexión añadida para usuario {user_id}")
+
+    async def disconnect_by_user(self, user_id):
+        # Cierra y elimina conexiones existentes de ese usuario
+        for conn in self.active_connections[:]:  # copia segura
+            if conn.user_id == user_id:
+                try:
+                    await conn.websocket.close()
+                except:
+                    pass
+                self.active_connections.remove(conn)
         
     def is_user_connected(self, user_id):
         return any(conn.user_id == user_id for conn in self.active_connections)
+    
+    def debug_active_connections(self):
+        print("=== CONEXIONES ACTIVAS ===")
+        for conn in self.active_connections:
+            print(f"Usuario: {conn.user_id}, Roles: {conn.roles}")
+        print("==========================")
 
     def disconnect(self, websocket: WebSocket):
         self.active_connections = [
             conn for conn in self.active_connections if conn.websocket != websocket
         ]
+        if websocket.client:
+            print(f"[WS] Conexión cerrada para {websocket.client.host}")
+        else:
+            print(f"[WS] Se cerró una conexión")
+        
+        print(f"[WS] Conexiones activas: {len(self.active_connections)}")
+        print(self.debug_active_connections())
 
     async def broadcast(self, message: dict, *, roles: Optional[List[str]] = None, user_id: Optional[int] = None, exclude: List[int] = []):
         data = json.dumps(message)
-        print(f"[WS] Enviando mensaje: {data}, a roles: {roles}, user_id: {user_id}, exclude: {exclude}")
-
+        print(f"[WS] Enviando mensaje: {data}")
+        print(self.debug_active_connections())
+        
+        # Enviar a todos los usuarios conectados
         for conn in self.active_connections:
             if conn.user_id in exclude:
                 continue
