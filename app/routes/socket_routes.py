0c22b5aa584bb45b613bc528d8fddb0672077e58
Alejo-P <marcelo.pinzon@epn.edu.ec>
2025-05-27 05:45:10 +0000

    Agregar manejo de excepciones en el endpoint WebSocket para mejorar la estabilidad y la gestión de desconexiones.

--- app/routes/socket_routes.py
+++ app/routes/socket_routes.py
@@ -1,5 +1,5 @@
 # app/main.py o app/routes/socket_routes.py
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect, Depends, HTTPException
 from sqlalchemy.orm import Session
 import json, asyncio
 
@@ -17,10 +17,10 @@ async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(get_db)
     WebSocket endpoint for notifications.
     """
     print("🔗 Conexión WebSocket iniciada")
-    await websocket.accept()
 
     try:
         user = await get_current_user_from_ws(websocket, db)
+        await websocket.accept()
 
         await manager.connect(
             websocket,
@@ -31,38 +31,38 @@ async def websocket_endpoint(websocket: WebSocket, db: Session = Depends(get_db)
         db.query(User).filter(User.id == user.id).update({User.is_connected: True})
         db.commit()
         print(f"✅ Usuario conectado: {user.id}")
+        await manager.broadcast({"event": "user_connected", "user_id": user.id}, roles=[ROLE_ADMIN], exclude=[user.id])
 
         while True:
             data = await websocket.receive_text()
             data_json = json.loads(data)
             print("📨 Mensaje recibido:", data_json)
             
-            if data_json["event"] == "user_connected":
-                # Aquí puedes manejar el evento de usuario conectado
-                print(f"Usuario conectado: {user.id}")
-                await manager.broadcast({"event": "user_connected", "user_id": user.id}, roles=[ROLE_ADMIN], exclude=[user.id])
+    except HTTPException as e:
+        print(f"🛑 Error autenticación WS: {e.detail}")
+        await websocket.close(code=1008)  # 1008 = Policy Violation
 
     except WebSocketDisconnect:
         print(f"❌ Usuario desconectado: {user.id}, se esperara 5 segundos antes de marcarlo como desconectado")
+        manager.disconnect(websocket)
         
         # Esperar 5 segundos antes de marcar al usuario como desconectado
         await asyncio.sleep(5)
         
         # Verificar si el usuario sigue conectado
+        print(manager.debug_active_connections())
         if not manager.is_user_connected(user.id):
             print(f"✅ Usuario desconectado: {user.id}")
             
             # Marcar al usuario como desconectado en la base de datos
             db.query(User).filter(User.id == user.id).update({User.is_connected: False})
             db.commit()
+            await manager.broadcast({"event": "user_disconnected", "user_id": user.id}, roles=[ROLE_ADMIN], exclude=[user.id])
         
-        manager.disconnect(websocket)
-        print(f"🔌 Desconectando WebSocket del usuario: {user.id}")
-        await manager.broadcast({"event": "user_disconnected", "user_id": user.id}, roles=[ROLE_ADMIN], exclude=[user.id])
 
     except Exception as e:
-        print("⚠️ Error en WebSocket:", str(e))
+        print(f"⚠️ Error inesperado: {str(e)}")
         try:
-            await websocket.close(code=1008)
-        except Exception as close_error:
-            print(f"WS close falló: {close_error}")
+            await websocket.close(code=1011)  # 1011 = Internal Error
+        except:
+            pass
